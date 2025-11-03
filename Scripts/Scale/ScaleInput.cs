75ba70e8362d0b5584985d4df849788e4ad2f386
DaBu Games <buitelaardaan2004@gmail.com>
2025-11-03 11:15:58 +0100

    scale rotate
    added the scale input to the scale rotate

--- Scripts/Scale/ScaleInput.cs
+++ Scripts/Scale/ScaleInput.cs
@@ -4,7 +4,7 @@ using WiimoteLib;
 using System.Text.RegularExpressions;
 using System.Threading;
 
-public partial class ScaleInput : Node
+public partial class ScaleInput : TextureRect
 {
     [Signal]
     public delegate void InitializedEventHandler();
@@ -237,9 +237,5 @@ public partial class ScaleInput : Node
         totalLeft = total * leftPerc;
         totalRight = total * rightPerc;
         totalWeight = total; 
-        
-        GD.Print($"Total Weight: {totalWeight:F2}");
-        GD.Print($"Total Left: {totalLeft:F2} percentage: {leftPerc:F2}");
-        GD.Print($"Total Right: {totalRight:F2} percentage: {rightPerc:F2}");
     }
 }
